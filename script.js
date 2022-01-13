0ee35e785cd95ab4a7294052881a63ab2b9ab122
Alejandro Rodriguez <alejanmrodriguez@gmail.com>
2022-01-13 12:54:46 -0500

    Created game function, still working on debugging ++computerScore, increments score even with a tie or erorr.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection,computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock' || playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper' || playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
         return 'You tied! Try again.';
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
@@ -21,10 +21,36 @@ function playRound(playerSelection,computerSelection) {
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
         return 'You lose! Scissors beats paper.';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        return 'You win! Scissors beats paper';
+        return 'You win! Scissors beats paper.';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
         return 'You lose! Rock beats scissors.';
     } else {
         return 'Something went horribly wrong.';
     }
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+    console.log(playerScore);
+    console.log(computerScore);
+    let playerSelection = prompt('Rock, paper, or scissors? Choose wisely.');
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection,computerSelection));
+    if (playRound(playerSelection, computerSelection) === 'You win! Rock beats scissors.' || playRound(playerSelection, computerSelection) === 'You win! Paper beats rock.' || playRound(playerSelection, computerSelection) === 'You win! Scissors beats paper.') {
+        ++playerScore;
+    } else if (playRound(playerSelection, computerSelection) !== 'You tied! Try again.' || playRound(playerSelection, computerSelection) !== 'Something went horribly wrong.') {
+        ++computerScore;
+    }
+    console.log(playerScore);
+    console.log(computerScore);
+    if (playerScore === 3) {
+        alert('You win best 3 out of 5!')
+    } else if (computerScore === 3) {
+        alert('You lose best 3 out of 5!')
+    }
+}
+
+game ();
