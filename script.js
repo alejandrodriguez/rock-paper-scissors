a98b1235be2141b0542bac85019321ac2d624e09
Alejandro Rodriguez <alejanmrodriguez@gmail.com>
2022-01-13 13:37:29 -0500

    Completed rock-paper-scissors project.

--- script.js
+++ script.js
@@ -33,24 +33,21 @@ let playerScore = 0;
 let computerScore = 0;
 
 function game() {
-    console.log(playerScore);
-    console.log(computerScore);
     let playerSelection = prompt('Rock, paper, or scissors? Choose wisely.');
     let computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
     console.log(playRound(playerSelection,computerSelection));
     if (playRound(playerSelection, computerSelection) === 'You win! Rock beats scissors.' || playRound(playerSelection, computerSelection) === 'You win! Paper beats rock.' || playRound(playerSelection, computerSelection) === 'You win! Scissors beats paper.') {
         ++playerScore;
-    } else if (playRound(playerSelection, computerSelection) !== 'You tied! Try again.' || playRound(playerSelection, computerSelection) !== 'Something went horribly wrong.') {
+    } else if (playRound(playerSelection, computerSelection) === 'You lose! Paper beats rock.' || playRound(playerSelection, computerSelection) === 'You lose! Scissors beats paper.' || playRound(playerSelection, computerSelection) === 'You lose! Rock beats scissors.') {
         ++computerScore;
     }
-    console.log(playerScore);
-    console.log(computerScore);
+    console.log('Score (Best of 5 Wins)\n\n' + 'You: ' + playerScore + '\nComputer: ' + computerScore);
     if (playerScore === 3) {
-        alert('You win best 3 out of 5!')
+        console.log('You win best 3 out of 5! Game Over.')
     } else if (computerScore === 3) {
-        alert('You lose best 3 out of 5!')
+        console.log('You lose best 3 out of 5! Game Over.')
+    } else {
+        game();
     }
 }
-
-game ();
