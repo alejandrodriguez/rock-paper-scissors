d43f69be3c5434c4c828b6b65ee994804107b548
Alejandro Rodriguez <alejanmrodriguez@gmail.com>
2022-01-13 11:23:07 -0500

    Created playRound function.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello World!')
+function computerPlay() {
+    let choice = Math.random();
+    if (choice <= 1 / 3) {
+        return 'rock';
+    } else if (choice <= 2/3) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock' || playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper' || playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+        return 'You tied! Try again.';
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors.';
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats rock.';
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock.';
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats paper.';
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper';
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats scissors.';
+    } else {
+        return 'Something went horribly wrong.';
+    }
+}
